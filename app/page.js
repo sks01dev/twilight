328eebce8f54592bc222bf33928ef89fe1b5ab68
sks01dev <sswarnkar0001@gmail.com>
2026-08-25 08:16:37 +0000

    updated the hero section of headline

--- app/page.js
+++ app/page.js
@@ -3,7 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <button className="btn">Get Started</button>
+      <h1>Collect customer feedback to build better products</h1>
+      <div>
+        Create a feedback board in minutes. Share it with your customers and
+        start collecting feedback.
+      </div>
     </main>
   );
 }
